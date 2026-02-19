e54cdac991254adad675ef28f67bd794ccfc0348
vartika <vartikav2005@gmail.com>
2026-02-20 01:55:30 +0530

    Backend authentication completed

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -8,9 +8,9 @@ exports.protect = (req, res, next) => {
 
   try {
     const token = header.split(" ")[1];
-    const decoded = jwt.verify(token, "internnova_secret");
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    req.user = decoded; // attach user info
+    req.user = decoded;
     next();
   } catch (err) {
     res.status(401).json({ message: "Invalid token" });
